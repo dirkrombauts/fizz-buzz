1b57aed342bfd79f5da6901679938171991dd711
Dirk Rombauts <dirk.rombauts@questmaster.net>
2014-10-27 11:02:58 +0100

    Add automation layer skeleton

---
+++ Specification/AutomationLayer/StepDefinitions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace FizzBuzz.Specification.AutomationLayer
+{
+  [Binding]
+  public class StepDefinitions
+  {
+    [Given(@"the current number is '(.*)'")]
+    public void GivenTheCurrentNumberIs(int p0)
+    {
+      ScenarioContext.Current.Pending();
+    }
+
+    [When(@"I print the number")]
+    public void WhenIPrintTheNumber()
+    {
+      ScenarioContext.Current.Pending();
+    }
+
+    [Then(@"the result is '(.*)'")]
+    public void ThenTheResultIs(int p0)
+    {
+      ScenarioContext.Current.Pending();
+    }
+  }
+}

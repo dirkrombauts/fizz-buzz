52e9fb4a7ee954d93e02860bd5adebdaad254e6d
Dirk Rombauts <dirk.rombauts@questmaster.net>
2014-10-27 11:21:21 +0100

    Implement Main logic

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,6 +6,12 @@ namespace FizzBuzz.ConsoleApp
   {
     static void Main()
     {
+      var fizzBuzzer = new FizzBuzzer();
+
+      for (int i = 1; i <= 100; i++)
+      {
+        Console.WriteLine(fizzBuzzer.Print(i));
+      }
     }
   }
 }
